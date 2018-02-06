14e7c2d0d37fa0d1d74d2e6bd6e243fd3ba75419
Daniel <danieljanderson0616@gmail,com>
2018-02-06 02:29:06 +0000

    made new file for just deck also wrote a shuffle function for the deck object

--- deck.js
+++ deck.js
@@ -1,4 +1,6 @@
-/*var SUITS = ['Clubs','Diamands','Hearts','Spades']
+var newDeck = (function(){
+ var deck = function (){ 
+ var SUITS = ['Clubs','Diamands','Hearts','Spades']
 var ORDER_VALUE = ['Ace','1','2','3','4','5','6','7','8','9','10','Jack','Qeen','King']
 // card is an object
 var card = {
@@ -7,15 +9,8 @@ var card = {
   image:'',
   flip_over:true 
 }
-//deck is an array of card objects
-
-  
-
-// shoe is an array of decks
-var shoe = []
-
-var createDeck = function(card){
-  var card_array=[]
+this.createDeck = function(card){
+   this.card_array=[]
   for (i=0;i<SUITS.length;i++){
     for(j=0; j<ORDER_VALUE.length;j++){
       var tempCard={flip_over:true}
@@ -23,15 +18,20 @@ var createDeck = function(card){
       tempCard.order_value=ORDER_VALUE[j]
       tempCard.image='image/'+tempCard.suit+tempCard.order_value
   
-      card_array.push(tempCard)
+      this.card_array.push(tempCard)
    
     }
   }
   
- return card_array
-  
+ return this.card_array
 }
 
-deck.cards=createDeck(card)
-console.log(deck)
-*/
+this.createDeck()
+}
+deck.prototype.shuffle = function(){
+ for (i=0;i<this.card_array.length;i++){
+   var temp=this.card_array.splice(i,1)
+   this.card_array.splice(Math.floor(Math.random()*this.card_array.lenth,0))
+ } 
+}
+})
