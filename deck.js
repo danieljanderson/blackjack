a90b63b0e13dbb0b0e6005db8a0fe82fd6f246b5
Daniel <danieljanderson0616@gmail,com>
2018-02-06 23:45:23 +0000

    deck created and displays correctly

--- deck.js
+++ deck.js
@@ -1,9 +1,9 @@
 var newDeck = (function(){
- var deck = function (){ 
- var SUITS = ['Clubs','Diamands','Hearts','Spades']
-var ORDER_VALUE = ['Ace','1','2','3','4','5','6','7','8','9','10','Jack','Qeen','King']
-// card is an object
-var card = {
+    var deck = function (){ 
+    var SUITS = ['Clubs','Diamands','Hearts','Spades']
+    var ORDER_VALUE = ['Ace','1','2','3','4','5','6','7','8','9','10','Jack','Qeen','King']
+    // card is an object
+    this.card = {
   suit :'',
   order_value:'',
   image:'',
@@ -11,13 +11,13 @@ var card = {
 }
 this.createDeck = function(card){
    this.card_array=[]
-  for (i=0;i<SUITS.length;i++){
+  for (i=0;i<SUITS.length-1;i++){
     for(j=0; j<ORDER_VALUE.length;j++){
-      var tempCard={flip_over:true}
+      var tempCard={}
       tempCard.suit=SUITS[i]
       tempCard.order_value=ORDER_VALUE[j]
       tempCard.image='image/'+tempCard.suit+tempCard.order_value
-  
+      tempCard.flip_over=true
       this.card_array.push(tempCard)
    
     }
@@ -26,12 +26,25 @@ this.createDeck = function(card){
  return this.card_array
 }
 
-this.createDeck()
-}
+//array.splice(index,how mmany,item1,another item, and last item)
+//index is required
+
 deck.prototype.shuffle = function(){
- for (i=0;i<this.card_array.length;i++){
-   var temp=this.card_array.splice(i,1)
-   this.card_array.splice(Math.floor(Math.random()*this.card_array.lenth,0))
- } 
+
+   
+ //this.card_array.splice(Math.floor(Math.random()*this.card_array.lenth-1),0,temp)
+  
 }
+ }
+ var test = function(){
+ var tempdeck = new deck()
+ tempdeck.createDeck()
+  console.log("new deck")
+  console.log(tempdeck.card_array)
+  console.log("shuffle deck")
+  tempdeck.shuffle()
+  console.log(tempdeck.card_array)
+ }
+ test()
 })
+newDeck()
