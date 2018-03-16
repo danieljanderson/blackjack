e06462c187d7f63d96915cbdc1cad6d6df1656ec
Daniel <danieljanderson0616@gmail,com>
2018-03-16 05:13:27 +0000

    works well.  only thing to fix is the get hand total function at the vary beginning of the program

--- blackjack.js
+++ blackjack.js
@@ -1,42 +1,52 @@
 //var DECK = require('./deck.js')
 //deck is global so i can just pass in the players for the hit function.  
+
 var blackjack = (function (num){
 	var game = function (num){
-	var DEALER_INDEX = -1
-	var current_turn = 0
-	// had to make deck a global varable because the function hi wouldnt have acess to the deck variable other wise.
-	var deck = createDeck(num)
-	this.playerArray =  []
+		var DEALER_INDEX = -1
+		var current_turn = 0
+		var deck = createDeck(num)
+		this.playerArray =  []
 		
-
+		// it takes a number calls the createPlayers method with a array of player objects the size of the numOfPlayers
+		// calls createDealer method.
+		// calls deal method cards to every player and the dealer
+		// and then gets the grand total of the two cards they have
 		this.startGame=function(numOfPlayers){
 			this.playerArray = createPlayers(numOfPlayers)
-		//	deck = createDeck(numOfDeck)	
 			deal(this.playerArray) 
 			this.dealer= createDealer()
+			for (var i =0;i<=this.playerArray.length;i++){
+					getHandTotal(this.playerArray[i].hand)
+					}
 		}
-	function deal(playerArray){
-	for (var i = 0; i<playerArray.length;i++){
-		for (var numCards = 0;numCards<2;numCards++){
-			playerArray[i].recieveCard(deck.draw())	
+	//this is a helper function that deals two cards to one player 
+		function deal(playerArray){
+			for (var i = 0; i<playerArray.length;i++){
+				for (var numCards = 0;numCards<2;numCards++){
+					playerArray[i].recieveCard(deck.draw())	
+				}
+			}
 		}
-	}
-	}
-	function createDealer(){
-		var dealer = new casino.Player()
-		dealer.name='dealer'
-		for (var numCards = 0;numCards<2;numCards++){
-					dealer.recieveCard(deck.draw())
-		}
+	
+	// this is a helper function that creates a new instance of the player object and assigns it to the variable dealer	
+		function createDealer(){
+			var dealer = new casino.Player()
+			dealer.name='dealer'
+			for (var numCards = 0;numCards<2;numCards++){
+				dealer.recieveCard(deck.draw())
+			}
 		return dealer
-	}
-	function createDeck (num){  
-     var blackjackDeck = new casino.Shoe(num)
-        for ( var i=0; i <= (num*5) ;i++){
-        	 blackjackDeck.shuffle()
-         }
-      return blackjackDeck
-        }
+		}
+	// this is a helper function that creates and returns a new instance of the shoe object.  it will then shuffle the shoe 5 times the number of decks that are in it	
+		function createDeck (num){  
+     	var blackjackDeck = new casino.Shoe(num)
+       	 for ( var i=0; i <= (num*5) ;i++){
+        		 blackjackDeck.shuffle()
+         	}
+    return blackjackDeck
+      }
+		//this is a helper function that returns an array of player objects that
 		function  createPlayers(num){
      	var arrayOfPlayers = []
 			for(var i=0;i<num;i++){
@@ -47,36 +57,37 @@ var blackjack = (function (num){
 		return arrayOfPlayers
 									  
      }
-			function getHandTotal(hand){
-    	var total = 0
-			var ace_count = 0
-			
-					for (var j=0;j<hand.length;j++){
-								//checks to see if the total is greater than 21.
-							isBust(total)
-							isTwentyOne(hand)		
-								// the if statement checks to see if the total is more than 21 and if so makes the ace 1.
-            if (hand[j].order==='King'||hand[j].order==='Queen'||hand[j].order==='Jack'){
-                   			 hand[j].value= 10
-               }
-              else if (hand[j].order ==='Ace'){
-                    		hand[j].value = 11
-											ace_count++
-               }
-              else{
-                    hand[j].value = Number(hand[j].order)
-              }
-            total = hand[j].value+total
-					}
+		// this is a helper function.  it takes a hand and returns a integer.
+		//it calculates the value of the players hand. if it is a hand that is over 21 and has aces it will then make the ace value of 1  
+		function getHandTotal(hand){
+    		var total = 0
+				var ace_count = 0
+				for (var j=0;j<hand.length;j++){
+						isBust(total)
+						isTwentyOne(hand)		
+          if (hand[j].order==='King'||hand[j].order==='Queen'||hand[j].order==='Jack'){
+              hand[j].value= 10
+           }
+          else if (hand[j].order ==='Ace'){
+         			hand[j].value = 11
+							ace_count++
+            }
+          else{
+          	 hand[j].value = Number(hand[j].order)
+           }
+     			total = hand[j].value+total
+			}
 				for(var i=0;i<ace_count;i++){
 					if (total <=21){
 						return total
-						}
+					}
 				total-=10
 				}
-          	return total
-      }
-			function changePlayers (listOfPlayers){
+  return total
+	}
+		// this is a helper function that changes the players.  it takes the players array and if their is a player to play they play.
+		//if the last player has gone it makes the current turn -1 meaning that its the dealers turn
+		function changePlayers (listOfPlayers){
 				if (current_turn == listOfPlayers.length-1){
 					current_turn=-1
 				}
@@ -84,93 +95,85 @@ var blackjack = (function (num){
 					current_turn++
 				}
 			}
-	this.stay= function(){
-		current_turn++
-	}	
-	this.hit= function (){
-		//toDo send player into get score function. 
-		//toDo get high score function only takes a hand and returns a total.  
-		//then then the hit function will see if its a bust
-		// if bust then you change players
-		//toDo create stay function that changes players
-		//toDo create a change player function and in there make sure that you incrument through the current_turn index
-		// in that function if its the length of the array make the index -1.
-		//toDo create dealer game function. and then that will lead to the end of the game
-		// create end of game function
-		console.log(this.dealer)
-	
-		//All the old functions should work just need to adapt it
-		//toDo create documentation in code for the whole program.
-		if (current_turn==-1){
-			dealerGame(this.dealer)
-		}
-		else{
-		var player= this.playerArray[current_turn]		
-
-		player.value=getHandTotal(player.hand)
-		player.recieveCard(deck.draw())
-		player.value=getHandTotal(player.hand)
-			if (player.value>22 && current_turn==-1){
-						endGame()
-						}
-			else if (player.value>22) {
+	//this is a stay function.  it just changes the currrent turn meaning it changes players	
+		this.stay= function(){
+			current_turn++
+		}	
+	//hit is a function that gives the current player a card
+	// it then will calculate the the current players total. 
+	//it will check to see if they have 21.
+	// it will then change players if the current players hand is greater than 21
+		this.hit= function (){
+			if (current_turn==-1){
+				dealerGame(this.dealer)
+				endGame(this.playerArray,this.dealer)
+			}
+			else{
+				var player= this.playerArray[current_turn]		
+				player.value=getHandTotal(player.hand)
+				player.recieveCard(deck.draw())
+				player.value=getHandTotal(player.hand)
+					if (player.value>22) {
 						changePlayers(this.playerArray)
 					}
 					
+		}
+		}
+	// this checks to see if they have 21
+		function isTwentyOne(hand){
+			if(hand[0].value===10||11){
+				if(hand[1].value===10||11){
+					return true
+				}
 			}
+		}
+	//this function sees if a players score is over 21 and returns true or false	
+		function isBust (score){
+			if (score>21){
+				return true
+			}
+			else{
+		 		return false
+			}
+		}
+	//this function takes a dealer object.  this will then give a dealer a card if they have 16 or less.  if its 17 the dealer will stay	
+		function dealerGame(dealer){
+			dealer.hand[0].flipOver=true
+			dealer.value=getHandTotal(dealer.hand)
+			while (dealer!==undefined){
+				if (dealer.value<17){
+					dealer.recieveCard(deck.draw())	
+					dealer.value=getHandTotal(dealer.hand)
+				}
+				else if(dealer.value>=17){
+				break
+				}
+				
+			}
+		}
+
+		// this function will then compare the players scores and will then store the results in the playerArray object
+		function endGame (playerArray,dealer){
+			for (var i = 0; i<=playerArray.length-1;i++){
+				if (isBust(dealer.value)){
+					playerArray[i].results = ""+playerArray[i].name+' you win'
+				}
+				else if(isBust(playerArray[i].value)){
+					playerArray[i].results =""+playerArray[i].name+' you busted you lose'
+					console.log("you busted")
+				}
+				else if(playerArray[i].value>dealer.value){
+					playerArray[i].results= " "+ playerArray[i].name+' you win'
+				}
+				else if(playerArray[i].value<dealer.value){
+					playerArray[i].results= " "+ playerArray[i].name+' you lose'	
+				}
+				else{
+					playerArray[i].results= " "+playerArray[i].name+' you push with the dealer'
+				}
+			}
+				
 	}
-	 function isTwentyOne(hand){
-			 if(hand[0].value===10||11){
-				 if(hand[1].value===10||11){
-					 return true
-				 }
-			 }
-		 }
-		 function isBust (score){
-			 if (score>21){
-				 return true
-			 }
-			 else{
-				 return false
-			 }
-		 }
-			function dealerGame(dealer){
-			
-				dealer.hand[0].flipOver=true
-				dealer.value=getHandTotal(dealer.hand)
-				while (dealer!==undefined){
-						if (dealer.value<17){
-							dealer.recieveCard(deck.draw())	
-							dealer.value=getHandTotal(dealer.hand)
-					
-						}
-						else if(dealer.value>=17){
-							endGame(dealer)
-						}
-				
-				}
-			}
-		
-			function endGame (dealer){
-				//dealer works but not the playerArray.  
-				console.log("the end game"+this.playerArray)
-				//-2 because dealer will always be listOfPlayers.1
-				for (var i = 0; i<=this.playerArray.length-1;i++){
-						if (isBust(this.dealer.hand)){
-							this.playerArray[i].results = ""+this.playerArray[i].name+'you win'
-						}
-						if(this.playerArray[i].value>this.dealer.value){
-							this.playerArray[i].results= " "+ this.playerArray[i].name+'you win'
-						}
-						else if(this.playerArray[i].value<this.dealer.value){
-						this.playerArray[i].results= " "+ this.playerArray[i].name+'you lose'	
-						}
-						else{
-						this.playerArray[i].results= " "+this.playerArray[i].name+'you push with the dealer'
-						}
-				}
-				
-			}
 
 	}
 	var module ={
@@ -182,5 +185,19 @@ var blackjack = (function (num){
 
 
 } )()
+/*
+QUESTIONS:
+1.why is line 18 undefined. the error says Uncaught TypeError: Cannot read property 'hand' of undefined but line 17 creates the players just fine
+i think it has to do with context.  this loses its context going into nested functions.  need help understand this an how to fix it
+2.how is it that the playerArray[i].results is getting saved in the playerArray object.  ex if you do the follow in the command line:
+var dan = new blackjack.game(8)
+dan.startGame(1)
+dan.hit() 
+dan.hit()
+and if you type in dan in the console it shows you that the results are.
+3. how do i fix question 1.
+
+
+*/
 
 		
