ffbe5e2d509f8a5f5a3222a210580469c203eed2
Daniel Anderson <30938196+danieljanderson@users.noreply.github.com>
2018-04-14 15:15:32 -0400

    Update deck.js

--- deck.js
+++ deck.js
@@ -2,25 +2,25 @@
 		var SUITS = ['Clubs','Diamands','Hearts','Spades']
     var ORDER_VALUE = ['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King']
     //creates a 52 card deck
-		function getCard(suit,value){
-				var singleCard={
-			 	'suit':suit,
-				'order':value,
-				'value':value,	
-				'flipOver':true,
-				'image':'image/'+suit+value+'.bmp'
-		 			}
-		return singleCard
-		 }
-		function Deck(){
-			this.cards = [] 
-			for (i=0;i<SUITS.length;i++){
-				for(j=0;j<ORDER_VALUE.length;j++){
-					var card = getCard(SUITS[i],ORDER_VALUE[j])
-					this.cards.push(card)	
-				}
+	function getCard(suit,value){
+		var singleCard={
+		'suit':suit,
+		'order':value,
+		'value':value,	
+		'flipOver':true,
+		'image':'image/'+suit+value+'.bmp'
+		}
+	return singleCard
+	}
+	function Deck(){
+		this.cards = [] 
+		for (i=0;i<SUITS.length;i++){
+			for(j=0;j<ORDER_VALUE.length;j++){
+			var card = getCard(SUITS[i],ORDER_VALUE[j])
+			this.cards.push(card)	
 			}
-			this.shuffle = function (){
+		}
+	this.shuffle = function (){
         var randomizedDeck = []
         while (this.cards.length!==0){
             var rIndex= Math.floor(this.cards.length * Math.random())
@@ -29,8 +29,8 @@
         }
          this.cards = randomizedDeck
     }
-			this.draw= function(){
-					return this.Deck.slice(1)
+	this.draw= function(){
+	return this.Deck.slice(1)
 			}
 		}		
 		
