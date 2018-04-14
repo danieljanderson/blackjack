6c33384d1e514a09aa70358d438b0edc3e55f5c6
Daniel Anderson <30938196+danieljanderson@users.noreply.github.com>
2018-04-14 15:17:55 -0400

    Update shoe.js

--- shoe.js
+++ shoe.js
@@ -2,24 +2,23 @@ casino.Shoe =
 
 	(function(num){
 	function Shoe(num){
-				this.Shoe = []
-				for ( var i= 0; i<num;i++){
-				
-					var tempDeck =new casino.Deck()
-					this.Shoe= this.Shoe.concat(tempDeck.cards)
-				}
-				this.shuffle = function (){
-        	var randomizedDeck = []
-        	while (this.Shoe.length!==0){
-            var rIndex= Math.floor(this.Shoe.length * Math.random())
-            randomizedDeck.push(this.Shoe[rIndex])
-             this.Shoe.splice(rIndex,1)
-        	}
-         this.Shoe = randomizedDeck
-				}
-				this.draw= function(){
-					return this.Shoe.splice(0,1)
-				}
+		this.Shoe = []
+		for ( var i= 0; i<num;i++){
+			var tempDeck =new casino.Deck()
+			this.Shoe= this.Shoe.concat(tempDeck.cards)
+			}
+		this.shuffle = function (){
+                var randomizedDeck = []
+                while (this.Shoe.length!==0){
+            		var rIndex= Math.floor(this.Shoe.length * Math.random())
+            		randomizedDeck.push(this.Shoe[rIndex])
+             		this.Shoe.splice(rIndex,1)
+        		}
+         	this.Shoe = randomizedDeck
+		}
+		this.draw= function(){
+		return this.Shoe.splice(0,1)
+		}
 		}
 
 	return Shoe	})()
